cd3f9d4100ab465e42db75ab4bfd594f63f860a9
James Gatannah <jamesgatannah@gmail.com>
2013-09-21 06:47:51 -0500

    Initial project creation via lein reloaded

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject talkiness "0.1.0-SNAPSHOT"
+  :description "TODO"
+  :url "TODO"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
+                   :source-paths ["dev"]}})
